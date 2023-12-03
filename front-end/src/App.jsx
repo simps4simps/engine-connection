4e31e4d364e8410a142295ddb6901c28e2ffc77c
simps4simps <savarimani6@gmail.com>
2023-12-03 17:35:52 +0330

    Made connection to socket and implemented "getShape" event

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Canvas from "./Components/Canvas/Canvas";
+import { io } from "socket.io-client";
 
 const App = () => {
+  useEffect(() => {
+    const socket = io("http://localhost:5000");
+
+    socket.emit("getShape", {
+      x: window.screenLeft,
+      y: window.screenTop,
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+  }, []);
   return (
     <div id="app-wrapper">
       <div id="main-display"></div>
