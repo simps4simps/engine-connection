4e29a9c7f3cb0483368d4ed766d84a1531b53b6c
simps4simps <savarimani6@gmail.com>
2023-12-03 14:08:45 +0330

    Added canvas

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./App.css";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div id="app-wrapper">
+      <canvas id="main-canvas"></canvas>
+    </div>
+  );
 };
 
 export default App;
