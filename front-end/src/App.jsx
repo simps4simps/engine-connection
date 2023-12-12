3afda881f0f98f7889642878840211bdb0998233
simps4simps <savarimani6@gmail.com>
2023-12-12 22:35:44 +0330

    Added ability to update and get windows data from window manager

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,17 +1,30 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./App.css";
 import Canvas from "./Components/Canvas/Canvas";
 import WindowManager from "./Assets/WindowManager/WindowManager";
 
 const App = () => {
+  const [data, setData] = useState();
+  const mainDisplay = useRef(null);
+
   useEffect(() => {
     const windowManager = new WindowManager("http://localhost:5000");
 
     windowManager.init("window");
+    windowManager.socket.on("sendData", (data) => {
+      setData(data);
+
+      setInterval(() => {
+        windowManager.update();
+      }, 600);
+    });
   }, []);
+
   return (
     <div id="app-wrapper">
-      <div id="main-display"></div>
+      <div id="main-display" ref={mainDisplay}>
+        <h1>{console.log(data)}</h1>
+      </div>
 
       <Canvas />
     </div>
